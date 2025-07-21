493cfd9e49a88149cff43f16f084db819f6551b7
boonmojae <pminny99@naver.com>
2025-07-21 16:21:38 +0900

    feat: schema

--- back/src/app.ts
+++ back/src/app.ts
@@ -7,8 +7,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//에러 미들웨어
+
+//라우터
+import authRouter from './routes/auth.routes';
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use("/auth", authRouter);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Book API Server' });
