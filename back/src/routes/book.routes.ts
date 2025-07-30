c1fdb871db8656dbc006df242aa38b5bb3b23217
boonmojae <pminny99@naver.com>
2025-07-30 22:55:49 +0900

    refactor: app.ts/bookDetail

--- back/src/routes/book.routes.ts
+++ back/src/routes/book.routes.ts
@@ -1,17 +1,10 @@
-// routes/book.routes.ts
 import express from 'express';
 import { bookController } from '../controllers/book.controller';
-// import { authenticate } from '../middlewares/authenticate'; 
-//const authenticate = (req: any, res: any, next: any) => next(); // 임시
 
 const router = express.Router();
 
-
 // 도서 리스트(메인) 도서 검색(aladin-api.service)
 router.get('/search', bookController.searchBooks);
-
-
-
 
 // 도서 리스트(aladin-api.service)
 // 베스트셀러
@@ -24,10 +17,7 @@ router.get('/newBooks', bookController.getNewBooks);
 router.get('/specialNewBooks', bookController.getSpecialNewBooks);
 
 
-
 // 도서 조회(book.service)
 router.get('/detail/:itemId', bookController.getBookDetail);
-
-
 
 export default router;
